d3c1ef5af2b40683b0f34f5ad8999e7249eefffc
P-e <36887778+P-eMartin@users.noreply.github.com>
2024-08-05 17:37:54 +0200

    after python session

--- Python/main.py
+++ Python/main.py
@@ -1,11 +1,34 @@
 ## import
 ## try to import this or antigravity
+# import this
+# import antigravity
 
 ## print() function
 ## print Hello World!
+# print('Hello World!')
 
 ## function definition and indentation:
 ## define your own function and call it
+# def my_function():
+#     global tmp
+#     tmp = 1
+#     print('Hello from my function!')
+#     # Print tmp
+#     print("tmp is", tmp)
+
+'''my_function()
+print("tmp is", tmp)
+tmp += 1
+print("tmp is", tmp)
+tmp = tmp + 1
+print("tmp is", tmp)
+'''
+
+# input_user = input("Enter a number: ")
+# print("You entered", input_user)
+# input_user = int(input_user)
+# input_user+=1
+# print("Now it's", input_user)
 
 ## Comments:
 ## use # to comment your code or triple quotes 
@@ -20,6 +43,25 @@
 ## input from user: use input() function
 
 ## list & numpy arrays
+## create a list and a numpy array
+
+# l = [1, 2, 3, 4, 5]
+# l2 = []
+# print("list l is", l)
+# for i in l:
+#     # add only even numbers
+#     if i % 2 == 0: # remainder of i divided by 2
+#         print(i)
+#         l2.append(2*i)
+#     else:
+#         l2.append(i)
+
+# print("list l2 is", l2)
+# import numpy as np
+# print("l2 - l:", np.array(l2) - np.array(l))
+
+# import numpy as np
+# a = np.array(l)
 
 ## loops and conditions
 
@@ -31,3 +73,7 @@
 ## 4. if not: display along the longest dimension a squared version of the image. (slowly or at the user convenience)
 ## 5. call the function in main.py with btg_2022.png and earth_temperature_timeline.png.
 ## If it freezes, try using ctrl + c (preferred) or ctrl + z to stop the execution.
+from utils import *
+
+image_path = 'Python/earth_temperature_timeline.png'
+display_squarred_image(image_path, wait=5)
--- Python/utils.py
+++ Python/utils.py
@@ -2,31 +2,43 @@ import cv2
 
 def display_squarred_image(image_path, wait=0):
     # Read image
+    img = cv2.imread(image_path)
 
     # Get image dimensions
+    height, width, channels = img.shape
+    print('Image dimensions:', height, 'x', width, 'x', channels)
 
     # Condition to check if image is square
-    if 1:
+    if height == width:
         # Display image
         print('Displaying image')
+        cv2.imshow('Image', img)
     else:
         # Check which dimension is the longest
-        if 1:
+        if width > height:
             print('Width is the longest dimension')
+            width_longest = True
+            longest_dim = width
+            shortest_dim = height
         else:
             print('Height is the longest dimension')
+            width_longest = False
+            longest_dim = height
+            shortest_dim = width
         # Loop along the longest dimension
-        for i in range(10):
+        for i in range(longest_dim-shortest_dim+1):
             # Using condition, crop the image along the longest dimension
-            if 1:
+            if width_longest:
                 print('Cropping along width')
+                img_show = img[:, i:shortest_dim+i]
             else:
                 print('Cropping along height')
+                img_show = img[i:shortest_dim+i, :]
             if wait == 0:
                 print('Press any key to continue')
             
             # Display the cropped image
-            
+            cv2.imshow('Image', img_show)
             # Wait key
             cv2.waitKey(wait)
             print('Progress: ', i+1, '/', 10)
